e91be247448364e4d6c984e537188846b55b6611
Daniel Felipe Vargas <d.vargasu@uniandes.edu.co>
2023-09-28 17:42:04 -0500

    todo funciona excepto verlo en la tabla

--- manejadorUsuarios/profesionales/urls.py
+++ manejadorUsuarios/profesionales/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("home", views.home, name="home"),
-    path("newDoctor", views.newDoctor, name="newDoctor"),
     path("submitDoctor", views.submitDoctor, name="submitDoctor"),
     path("getDoctors", views.getDoctors, name="getDoctors"),
 ]
