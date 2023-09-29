036de59698356d9179a494b4e2cd4209cf802cc2
JuanCoronel70 <146017623+JuanCoronel70@users.noreply.github.com>
2023-09-29 13:31:16 -0500

    Update settings.py

--- manejadorUsuarios/manejadorUsuarios/settings.py
+++ manejadorUsuarios/manejadorUsuarios/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "manejadorUsuarios.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "rasi_db",
+        "USER": "rasi_user",
+        "PASSWORD": "rasi",
+        "HOST": '10.99.144.3',
+        "PORT": '',
     }
 }
 
