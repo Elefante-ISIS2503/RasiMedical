1a29bac59d2f4d36cb0a59259edfb1cc5436919b
ElSergioOliveros <s.oliverosb@uniandes.edu.co>
2023-11-17 11:09:47 -0500

    ip

--- appCliente/appCliente/settings.py
+++ appCliente/appCliente/settings.py
@@ -137,4 +137,4 @@ SOCIAL_AUTH_AUTH0_KEY = 'MGz0z6kFDjO7s3iIreg4J5HZpXhhiaBb'
 SOCIAL_AUTH_AUTH0_SECRET = '2oj_por1UApZ-YkHsknNgdmmxfUUl2UhYnm3y-Enc9Cr--vQlEp0Be_hBvAXY0ro'
 
 SOCIAL_AUTH_AUTH0_SCOPE = [ 'openid', 'profile','email','role', ]
-AUTHENTICATION_BACKENDS = { 'monitoring.auth0backend.Auth0', 'django.contrib.auth.backends.ModelBackend'}
+AUTHENTICATION_BACKENDS = { 'appCliente.auth0backend.Auth0', 'django.contrib.auth.backends.ModelBackend'}
