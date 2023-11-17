6e317779af7602acffe1dac0833154592b1a7368
ElSergioOliveros <s.oliverosb@uniandes.edu.co>
2023-11-16 22:26:04 -0500

    inicio auth0

--- appCliente/appCliente/settings.py
+++ appCliente/appCliente/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "ui",
+    "social_django"
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ipInstanciaPublica = ""
+auth0Domain = "isis2503-elsergiooliveros.us.auth0.com"
+
+# AUTH0 STUFF
+LOGIN_URL = "/login/auth0" 
+LOGIN_REDIRECT_URL = "/" 
+LOGOUT_REDIRECT_URL = f"https://{auth0Domain}/v2/logout?returnTo=http%3A%2F%2F{ipInstanciaPublica}:8080"
+SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
+SOCIAL_AUTH_AUTH0_DOMAIN = f"{auth0Domain}" 
+SOCIAL_AUTH_AUTH0_KEY = 'MGz0z6kFDjO7s3iIreg4J5HZpXhhiaBb' 
+SOCIAL_AUTH_AUTH0_SECRET = '2oj_por1UApZ-YkHsknNgdmmxfUUl2UhYnm3y-Enc9Cr--vQlEp0Be_hBvAXY0ro'
+
+SOCIAL_AUTH_AUTH0_SCOPE = [ 'openid', 'profile','email','role', ]
+AUTHENTICATION_BACKENDS = { 'monitoring.auth0backend.Auth0', 'django.contrib.auth.backends.ModelBackend'}
--- appCliente/appCliente/urls.py
+++ appCliente/appCliente/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("ui.urls")),
+    path(r'', include('django.contrib.auth.urls')),
+    path(r'', include('social_django.urls')),
 ]
