5ad09f70c9b7ba42ae80ae4f6d0777acf4d5c3b3
ElSergioOliveros <s.oliverosb@uniandes.edu.co>
2023-12-05 23:28:13 -0500

    ip

--- appCliente/appCliente/settings.py
+++ appCliente/appCliente/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # AUTH0 STUFF
-ipInstanciaPublica = "34.72.13.230"
+ipInstanciaPublica = "104.198.246.198"
 auth0Domain = "isis2503-elsergiooliveros.us.auth0.com"
 
 LOGIN_URL = "/login/auth0" 
